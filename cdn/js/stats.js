e569c5b0aa7bd186e9c4239e6c97f5cfa9428d2a
Emilio <emirodgar@gmail.com>
2019-05-08 11:48:45 +0200

    Internal Analytics test

--- cdn/js/stats.js
+++ cdn/js/stats.js
@@ -1,5 +1,5 @@
 <script>
-var url = window.location;
+var url = document.URL;
 var data = "https://docs.google.com/forms/d/e/1FAIpQLSfdvQc52CaFwyDZUpB3srNSewb56CUt8jbN53dNaAOdn-Fz1g/formResponse?usp=pp_url&entry.1541732625="+url+"&submit=SUBMIT";
   
 $.getJSON(data,function(data){});
