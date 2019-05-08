b2ac1b7ba3c816435f626bbd8ce6c34ad9ff9cce
Emilio <emirodgar@gmail.com>
2019-05-08 11:54:50 +0200

    Internal Analytics test

--- cdn/js/statsgs.js
+++ cdn/js/statsgs.js
@@ -1,3 +1,8 @@
+<script
+  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+  crossorigin="anonymous">
+</script>
 <script>
 var url = document.URL;
 var data = "https://docs.google.com/forms/d/e/1FAIpQLSfdvQc52CaFwyDZUpB3srNSewb56CUt8jbN53dNaAOdn-Fz1g/formResponse?usp=pp_url&entry.1541732625="+url+"&submit=SUBMIT";
